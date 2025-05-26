9ce017dab57c59748831add8ec172568831f7154
AvantikaPatel <avantikakurmi2012@gmail.com>
2025-05-26 18:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+  event.preventDefault();
+  let fName = document.getElementById("fname").value.trim();
+  let lName = document.getElementById("lname").value.trim();
 
+  if (fName === "" || lName === "") {
+    alert("Please enter both first and last name.");
+    return;
+  }
+  alert(fName + " " + lName);
 }
